6a1e451dc311720ea60e0d2efbb153ab86cbe0ea
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-05-01 11:16:17 +0200

    fix: correzione console.log 3

--- script.js
+++ script.js
@@ -129,8 +129,10 @@
 // Senza lanciare il codice, riesci a prevedere cosa viene stampato in console?
 // console.log(hamburger.maker.name); // Chef Hyur
 // console.log(secondBurger.maker.name); // Chef Hyur
-// console.log(hamburger.maker.restaurant.name); // Hyur's Burgers
+// console.log(hamburger.maker.restaurant.name); // Hyur's II
 // console.log(secondBurger.maker.restaurant.name); // Hyur's II
 
 // Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice?
 // 7 oggetti
+
+// correzione al console.log 3, perché "secondBurger.maker.restaurant = newRestaurant;" cambia l'originale restaurant in "hamburger"
