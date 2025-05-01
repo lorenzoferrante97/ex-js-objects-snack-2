88fd3ed44ae9ec80354985691304d2153a929b1c
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-05-01 10:52:28 +0200

    add: risposte question 3

--- script.js
+++ script.js
@@ -41,3 +41,27 @@
 
 // Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice?
 // due oggetti, ma l'array di riferimento "ingredients" è uno
+
+// QUESTION 3 -----------------------------
+
+// const hamburger = {
+// 	name: "Cheese Burger",
+// 	weight: 250,
+// 	maker: {
+// 		name: "Anonymous Chef",
+// 		restaurant: {
+// 			name: "Hyur's Burgers",
+// 			address: "Main Street, 123",
+// 			isOpen: true,
+// 		},
+// 		age: 29
+// 	}
+// };
+
+// const secondBurger = structuredClone(hamburger);
+// const thirdBurger = structuredClone(hamburger);
+
+// RISPOSTE -----------
+
+// Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice?
+// in totale sono 9 oggetti
