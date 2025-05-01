3222777f8136c82f23a07bcc8cfd1fc60216bb03
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-05-01 10:48:50 +0200

    add: risposte question 2

--- script.js
+++ script.js
@@ -18,3 +18,26 @@
 
 // Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice?
 // solo un oggetto
+
+// QUESTION 2 -----------------------------
+
+// const hamburger = {
+// 	name: "Cheese Burger",
+// 	weight: 250,
+// 	ingredients: ["Cheese", "Meat", "Bread", "Tomato"]
+// };
+
+// const secondBurger = {...hamburger};
+// secondBurger.ingredients[0] = "Salad";
+
+// console.log(hamburger.ingredients[0]); // ?
+// console.log(secondBurger.ingredients[0]); // ?
+
+// RISPOSTE -----------
+
+// Senza lanciare il codice, riesci a prevedere cosa viene stampato in console?
+// console.log(hamburger.ingredients[0]); -> Salad
+// console.log(secondBurger.ingredients[0]); -> Salad
+
+// Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice?
+// due oggetti, ma l'array di riferimento "ingredients" è uno
