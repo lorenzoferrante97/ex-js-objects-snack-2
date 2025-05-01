8f2a4196d2bc67df200b9588b091beb3e4ee6695
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-05-01 11:13:54 +0200

    add: risposte question 5

--- script.js
+++ script.js
@@ -93,3 +93,44 @@
 
 // Qual è il metodo migliore per clonare l’oggetto restaurant, e perché?
 // lo structuredClone, in quanto in "restaurant" c'è un Date(), quindi permette di mantenerlo come tale, e non ci sono metodi
+
+// BONUS ------------------------------------------------
+
+// QUESTION 5 -----------------------------
+
+// const hamburger = {
+// 	name: "Cheese Burger",
+// 	weight: 250,
+// 	maker: {
+// 		name: "Anonymous Chef",
+// 		restaurant: {
+// 			name: "Hyur's Burgers",
+// 			address: "Main Street, 123",
+// 			isOpen: true,
+// 		},
+// 		age: 29
+// 	}
+// };
+
+// const newRestaurant = {...hamburger.maker.restaurant};
+// newRestaurant.name = "Hyur's II";
+// newRestaurant.address = "Second Street, 12";
+// const secondBurger = {...hamburger};
+// secondBurger.maker.restaurant = newRestaurant;
+// secondBurger.maker.name = "Chef Hyur";
+
+// console.log(hamburger.maker.name); // ?
+// console.log(secondBurger.maker.name); // ?
+// console.log(hamburger.maker.restaurant.name); // ?
+// console.log(secondBurger.maker.restaurant.name); // ?
+
+// RISPOSTE -----------
+
+// Senza lanciare il codice, riesci a prevedere cosa viene stampato in console?
+// console.log(hamburger.maker.name); // Chef Hyur
+// console.log(secondBurger.maker.name); // Chef Hyur
+// console.log(hamburger.maker.restaurant.name); // Hyur's Burgers
+// console.log(secondBurger.maker.restaurant.name); // Hyur's II
+
+// Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice?
+// 7 oggetti
